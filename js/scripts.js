223ee99ae08328a33db95e121f00e2391b0b4a8d
luccasr73 <luccasr83@gmail.com>
2017-08-11 12:30:01 -0300

    Fix autoplay mobile

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,4 @@ var audio = document.getElementById("audio");
 function tocamusica() { 
     audio.play(); 
 } 
-audio.onscroll = function() {tocamusica()};
-audio.ontouchstart = function() {tocamusica()};
-audio.onclick = function() {tocamusica()};
+
